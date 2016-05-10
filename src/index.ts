359073a5c2e6b184162cd4dd76fba367c9dcb78a
Astha Sharma <a@astha.me>
2016-05-11 00:15:07 +0530

    Rename render to renderBoard

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 'use strict'
 
 import Board from './core/board/model'
-import render from './core/board/ui'
+import renderBoard from './core/board/ui'
 
 const board = new Board()
 const mount = document.getElementById('board')
 
-render(board, mount)
+renderBoard(board, mount)
