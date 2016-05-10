5ef901f7a1a5d9d2cf0f3bfd055e4ecb03239889
Astha Sharma <a@astha.me>
2016-05-10 17:04:54 +0530

    Remove node typings

--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/node-uuid/index.d.ts" />
-/// <reference path="main/ambient/node-uuid/node-uuid-base/index.d.ts" />
-/// <reference path="main/ambient/node-uuid/node-uuid-cjs/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
