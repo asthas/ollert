1cc8200ff5f1da2930303273e2abd12a18b8443d
Astha Sharma <a@astha.me>
2016-05-12 00:02:12 +0530

    Made the cards editable

--- src/core/card/ui.ts
+++ src/core/card/ui.ts
@@ -1,18 +1,34 @@
 'use strict'
 
 import Card from './model'
+import Event from '../../utils/events';
 
 const render = (card: Card) => {
     var save = document.createElement('button')
+    save.className = 'save'
     save.innerText = 'Save'
+
     const cardUi = document.createElement('div')
     cardUi.className = 'card'
     cardUi.innerText = card.title
-    cardUi.onclick = function(){
+    cardUi.onclick = () => {
         const editCard = document.createElement('div')
-        this.contentEditable = true
+        const cardarea = document.createElement('textarea')
+        cardarea.className = 'cardarea'
+        cardarea.setAttribute('rows', '3')
+        cardarea.value = card.title
+        var save = document.createElement('button')
+        save.className = 'save'
+        save.innerText = 'Save'
+        editCard.appendChild(cardarea)
         editCard.appendChild(save)
+        cardUi.parentElement.replaceChild(editCard, cardUi)
+        save.onclick = () => {
+            card.title = cardarea.value
+            Event.rerender()
+        }
     }
+
     return cardUi
 }
 
