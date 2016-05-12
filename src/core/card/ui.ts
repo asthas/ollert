2d84d9586d79f2fd24cc553a5dbf931c593eea51
Astha Sharma <a@astha.me>
2016-05-12 10:34:41 +0530

    Added pencil icon

--- src/core/card/ui.ts
+++ src/core/card/ui.ts
@@ -4,14 +4,13 @@ import Card from './model'
 import Event from '../../utils/events';
 
 const render = (card: Card) => {
-    var save = document.createElement('button')
-    save.className = 'save'
-    save.innerText = 'Save'
 
     const cardUi = document.createElement('div')
     cardUi.className = 'card'
-    cardUi.innerText = card.title
-    cardUi.onclick = () => {
+    cardUi.innerText += card.title
+    cardUi.innerHTML += '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>'
+    const icon = document.getElementsByClassName('fa-pencil-square-o')[0] as HTMLElement
+    icon.onclick = () => {
         const editCard = document.createElement('div')
         const cardarea = document.createElement('textarea')
         cardarea.className = 'cardarea'
